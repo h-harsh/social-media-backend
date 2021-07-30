6b495268c36b8f7a2755e91945d6f78067352313
h-harsh <porwalharshp@gmail.com>
2021-07-31 01:06:56 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('This is the backend for socila media')
+});
+
+app.listen(process.env.PORT ||port, () => {
+  console.log(` app listening on port ${port}!`)
+});
